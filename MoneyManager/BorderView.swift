946b53fe5f0729e91a286584e21531686f26e06f
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2018-01-28 13:32:55 +0200

    initial layouts

---
+++ MoneyManager/BorderView.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  MoneyManager
+//
+//  Created by saba rezvan on 28/01/2018.
+//  Copyright © 2018 hulkdx. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class BorderView : UIView {
+    @IBInspectable var borderColor: UIColor = .clear {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+}
